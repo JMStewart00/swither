34327c255aac1b29d2a314fc53c538c41d775e96
JMStewart00 <jmstewart00@gmail.com>
2017-08-01 08:51:01 -0400

    removed redirect after login for backend

--- routes/web.php
+++ routes/web.php
@@ -18,20 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/index', 'YelpController@index');
-
-Route::get('/callback', function (Illuminate\Http\Request $request) {
-    $http = new GuzzleHttp\Client;
-
-	$response = $http->post('http://localhost:7000/oauth/token', [
-	    'form_params' => [
-	        'grant_type' => 'password',
-	        'client_id' => '1',
-	        'client_secret' => 'VWaDOa2bPQH1iiYyMfFER6wFAAWjNyBAGfSE1uK6',
-	        'username' => 'josh@example.com',
-	        'password' => 'justlax1',
-	        'scope' => '',
-	    ],
-	]);
-
-return json_decode((string) $response->getBody(), true);
-});
