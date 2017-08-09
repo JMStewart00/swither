e8dd599bc28e0e8e403f082d828fd353c15a90d9
JMStewart00 <jmstewart00@gmail.com>
2017-08-09 14:40:44 -0400

    fixing api route for likes

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -12,7 +12,7 @@ class LikesController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(Request $request)
+    public function getLikes(Request $request)
     {
         $user = $request->user_id;
         $group = $request->group_id;
